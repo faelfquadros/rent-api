1f3ee37123bf2fa80ae577d0bfcab2d20e716cb9
rafaelfquadros <rafael.rffq@gmail.com>
2022-06-13 21:27:31 -0300

    feat: :sparkles: Create Category supertest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,71 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { AppError } from "@shared/errors/AppError";
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(`
+       insert into users (id, name, email, password, admin, created_at, driver_license)
+       values('${id}', 'admin', 'admin@rentapi.com', '${password}', true, 'now()', 'XXXXXX')`);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("shoud be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentapi.com",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category super test name",
+        description: "Category super test description",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("shoud not be able to create a new category if name already exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentapi.com",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category super test name",
+        description: "Category super test description",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(409);
+  });
+});
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,3 @@
-import "reflect-metadata";
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import createConnection from "@shared/infra/typeorm";
-
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
-
-import "@shared/container";
-
-createConnection();
-const app = express();
-
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(express.json());
-app.use(router);
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: "error",
-    message: `Internal server error: ${error}`,
-  });
-});
+import { app } from "./app";
 
 app.listen(3000, () => console.log("Its Working !! 🔥"));
