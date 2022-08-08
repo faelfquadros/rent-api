2f4bd9077a0359bf4e0c4a561b1f5fd57c8ca042
rafaelfquadros <rafael.rffq@gmail.com>
2022-08-08 20:57:56 -0300

    feat: :sparkles: reset password route

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailController";
 
 const passwordRoutes = Router();
@@ -7,5 +8,8 @@ const passwordRoutes = Router();
 const sendForgotPasswordController = new SendForgotPasswordMailController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordController.handle);
+
+const resetPasswordUserController = new ResetPasswordUserController();
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
